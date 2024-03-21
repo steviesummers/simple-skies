0d1ce4753b163cde9d572b708c8c2dee1c08b6df
lifewawj <arevaloaj01@gmail.com>
2024-03-21 16:33:04 -0700

    Added CSS Library style-components and changed App, Footer, Header styles by seperating it from the return for more easy future changing the styles

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,24 +1,90 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom"; // Import the Link tag, to help with navigating to routes without page reload
+import Auth from "../../utils/auth"; // Imports our Authorization
+import { styled, keyframes } from "styled-components"; // Imports the styled variabke from the styled-components CSS library
+import colors from "../colors"
 
-import Auth from '../../utils/auth';
 
+// styled-components
+// Create the keyframes
+const rotate = keyframes`
+  from {
+    transform: (0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const Rotate = styled.div`
+  display: inline-block;
+  animation: ${rotate} 3s linear infinite;
+  padding: 2rem 1rem;
+  font-size: 5rem;
+`;
+const pulse = keyframes`
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.1);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+`;
+const Pulse = styled.div`
+  animation: ${pulse} 2s infinite;
+`;
+
+const H1 = styled.h1`
+  color: ${colors.textColor};
+  font-size: 3rem;
+`;
+const Button = styled.button`
+  padding: 10px 25px;
+  color: ${colors.btnTextColor};
+  background: #44f49c;
+  border-radius: 5px;
+  border-color: ${colors.btnBackgroundColor}
+`;
+const Heading = styled.header`
+  background: #1e1e1e;
+  color: ${colors.textColor};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const P = styled.p`
+  colors: ${colors.textColor};
+  font-size: 1rem;
+`
+
+
+// The returned Header
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Tech Friends
-          </h1>
+    <Heading> 
+        {/* Logo when clicked leads back to "/" route */}
+        <Link to="/">
+        <Rotate> 🌎 </Rotate>
         </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Meet your new programming pals.
-        </p>
+        <Link to="/">
+          <H1 > Ur-ni-Earth Teller </H1>
+        </Link>
+        {/* Description of our Web-App. */}
+        <P>
+          As an individual who contemplates my place in the universe, I want to see pictures of the Earth on given dates so that when I look back on this little life, I can see that pale blue dot on a set on given dates.
+        </P>
+      <div>
         <div>
+          {/* If logged in, display this */}
           {Auth.loggedIn() ? (
             <>
               <Link className="btn btn-lg btn-primary m-2" to="/me">
@@ -29,18 +95,19 @@ const Header = () => {
               </button>
             </>
           ) : (
+            // If NOT logged in, display this
             <>
-              <Link className="btn btn-lg btn-primary m-2" to="/login">
-                Login
+              <Link to="/login">
+                <Button>Login</Button>
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
+              <Link to="/signup">
+                <Button>Signup</Button>
               </Link>
             </>
           )}
         </div>
       </div>
-    </header>
+    </Heading>
   );
 };
 
