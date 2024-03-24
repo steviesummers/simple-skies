f994180ad0f32d19ede4fea178462a4b4bbb2365
wmason1997 <williamcmason1997@gmail.com>
2024-03-24 10:41:13 -0700

    Edited homepage for a date intake

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 
 import ProfileList from '../components/ProfileList';
@@ -5,13 +6,36 @@ import ProfileList from '../components/ProfileList';
 import { QUERY_PROFILES } from '../utils/queries';
 
 const Home = () => {
+  const [date, setDate] = useState('');
   const { loading, data } = useQuery(QUERY_PROFILES);
   const profiles = data?.profiles || [];
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Handle submission logic here
+    console.log('Submitted date:', date);
+    // You can add your logic here to perform any actions with the date
+  };
 
   return (
     <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 my-3">
+          <div className="input-group mb-3">
+            <input
+              type="date"
+              className="form-control"
+              placeholder="Select a date"
+              value={date}
+              onChange={handleDateChange}
+            />
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
           {loading ? (
             <div>Loading...</div>
           ) : (
